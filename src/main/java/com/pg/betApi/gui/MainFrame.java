81e1a84d0ca6a304556fc6ad747db1dc0c01c85e
傅士勇 <nengbowan@163.com>
2019-11-06 13:34:01 +0800

    gui commit

--- src/main/java/com/pg/betApi/gui/MainFrame.java
+++ src/main/java/com/pg/betApi/gui/MainFrame.java
@@ -5,17 +5,17 @@ import java.awt.*;
 
 public class MainFrame extends JFrame {
     //上子组件 投注配置组件
-    private JPanel topPanel;
+    private JPanel topPanel = new JPanel(new FlowLayout());
     //下子组件 日志组件
-    private JPanel bottomPanel;
+    private JPanel bottomPanel = new JPanel();;
 
-    private JPanel loginPanel;
+    private JPanel loginPanel = new JPanel(new FlowLayout());;
     //创建选择投注号码位置 1 2 3 4 5 6 7 8 9 10 选择那一条马
-    private JPanel horseNumberPanel;
+    private JPanel horseNumberPanel = new JPanel(new FlowLayout());;
     public MainFrame(){
         //左上角
         this.setLocation(0,0);
-        //设置不可扩大 缩小界面 TODO
+        //设置不可扩大 缩小界面
         this.setResizable(true);
         //设置窗口 宽 高
         this.setSize(950,500);
@@ -55,13 +55,10 @@ public class MainFrame extends JFrame {
     }
 
     private void initBottomPanel() {
-        bottomPanel = new JPanel();
+
     }
 
     private void initTopPanel() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new FlowLayout());
-        this.topPanel = panel;
         //创建登录账号密码配置区域 username password
         createLoginPanel();
         //创建选择投注号码位置 1 2 3 4 5 6 7 8 9 10 选择那一条马
@@ -73,43 +70,31 @@ public class MainFrame extends JFrame {
     }
 
     private void createHorseNumberPanel() {
-        JLabel number1 = new JLabel("1");
-        JLabel number2 = new JLabel("2");
-        JLabel number3 = new JLabel("3");
-        JLabel number4 = new JLabel("4");
-        JLabel number5 = new JLabel("5");
-        JLabel number6 = new JLabel("6");
-        JLabel number7 = new JLabel("7");
-        JLabel number8 = new JLabel("8");
-        JLabel number9 = new JLabel("9");
-        JLabel number10 = new JLabel("10");
+        horseNumberPanel.setBorder(BorderFactory.createTitledBorder("跑马选择:"));
 
-        JPanel horseNumberPanel = new JPanel(new FlowLayout());
+        JCheckBox item1 = new JCheckBox("1");
+        JCheckBox item2 = new JCheckBox("2");
+        JCheckBox item3 = new JCheckBox("3");
+        JCheckBox item4 = new JCheckBox("4");
+        JCheckBox item5 = new JCheckBox("5");
+        JCheckBox item6 = new JCheckBox("6");
+        JCheckBox item7 = new JCheckBox("7");
+        JCheckBox item8 = new JCheckBox("8");
+        JCheckBox item9 = new JCheckBox("9");
+        JCheckBox item10 = new JCheckBox("10");
 
-        JCheckBox numberCheckBox = new JCheckBox();
-        JCheckBoxMenuItem item1 = new JCheckBoxMenuItem("1");
-        JCheckBoxMenuItem item2 = new JCheckBoxMenuItem("2");
-        JCheckBoxMenuItem item3 = new JCheckBoxMenuItem("3");
-        JCheckBoxMenuItem item4 = new JCheckBoxMenuItem("4");
-        JCheckBoxMenuItem item5 = new JCheckBoxMenuItem("5");
-        JCheckBoxMenuItem item6 = new JCheckBoxMenuItem("6");
-        JCheckBoxMenuItem item7 = new JCheckBoxMenuItem("7");
-        JCheckBoxMenuItem item8 = new JCheckBoxMenuItem("8");
-        JCheckBoxMenuItem item9 = new JCheckBoxMenuItem("9");
-        JCheckBoxMenuItem item10 = new JCheckBoxMenuItem("10");
 
-        numberCheckBox.add(item1);
-        numberCheckBox.add(item2);
-        numberCheckBox.add(item3);
-        numberCheckBox.add(item4);
-        numberCheckBox.add(item5);
-        numberCheckBox.add(item6);
-        numberCheckBox.add(item7);
-        numberCheckBox.add(item8);
-        numberCheckBox.add(item9);
-        numberCheckBox.add(item10);
+        horseNumberPanel.add(item1);
+        horseNumberPanel.add(item2);
+        horseNumberPanel.add(item3);
+        horseNumberPanel.add(item4);
+        horseNumberPanel.add(item5);
+        horseNumberPanel.add(item6);
+        horseNumberPanel.add(item7);
+        horseNumberPanel.add(item8);
+        horseNumberPanel.add(item9);
+        horseNumberPanel.add(item10);
 
-        horseNumberPanel.add(numberCheckBox);
 
         topPanel.add(horseNumberPanel);
     }
@@ -118,20 +103,31 @@ public class MainFrame extends JFrame {
      * 创建登录账号密码配置区域 username password
      */
     private void createLoginPanel() {
-        JPanel loginPanel = new JPanel(new FlowLayout());
 
+
+        loginPanel.setBorder(BorderFactory.createTitledBorder("账号配置:"));
         JLabel username = new JLabel("账号:");
         JLabel password = new JLabel("密码:");
 
         JTextField userText = new JTextField(20);
         JTextField passText = new JTextField(20);
 
-        loginPanel.add(username);
-        loginPanel.add(userText);
-        loginPanel.add(password);
-        loginPanel.add(passText);
-        this.loginPanel = loginPanel;
 
+        Box usernameBox = Box.createHorizontalBox();
+        Box passwdBox = Box.createHorizontalBox();
+        usernameBox.add(username);
+        usernameBox.add(userText);
+
+        passwdBox.add(password);
+        passwdBox.add(passText);
+
+
+        Box loginBox = Box.createVerticalBox();
+        loginBox.add(usernameBox);
+        loginBox.add(passwdBox);
+
+        loginPanel.add(loginBox);
+        topPanel.add(loginPanel);
     }
 
 
